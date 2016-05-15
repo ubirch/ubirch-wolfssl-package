37f5ad2ec89fa3a3adf3413167ca9d864739518f
Matthias L. Jugel <leo@thinkberg.com>
2016-05-15 23:34:43 +0200

    build only MinSizeRel by default

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #! /bin/sh
 TOOLCHAIN=../ubirch-arm-toolchain/cmake/ubirch-arm-gcc-toolchain.cmake
 # do an out-of-source build for all configurations
+if [ "$1" == "-a" ]
+then BUILDS="Debug Release MinSizeRel RelWithDebInfo"
+else BUILDS=MinSizeRel
+fi
 [ -d build ] && rm -r build
-for BUILD_TYPE in Debug Release MinSizeRel RelWithDebInfo
+for BUILD_TYPE in $BUILDS
 do
   (mkdir -p build/$BUILD_TYPE; cd build/$BUILD_TYPE; \
   cmake ../.. -DCMAKE_TOOLCHAIN_FILE="../../$TOOLCHAIN" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"; \
