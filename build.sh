a4dfc7382f581cbd0bd496769be1dfc13e09e03a
Matthias L. Jugel <leo@thinkberg.com>
2016-05-16 20:22:15 +0200

    fix MCU handling

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #! /bin/sh
 TOOLCHAIN=../ubirch-arm-toolchain/cmake/ubirch-arm-gcc-toolchain.cmake
+MCU=MK82F25615
 # do an out-of-source build for all configurations
 if [ "$1" == "-a" ]
 then BUILDS="Debug Release MinSizeRel RelWithDebInfo"
 else BUILDS=MinSizeRel
 fi
-[ -d build ] && rm -r build
+[ -d "build/$MCU" ] && rm -r "build/$MCU"
 for BUILD_TYPE in $BUILDS
 do
-  (mkdir -p build/$BUILD_TYPE; cd build/$BUILD_TYPE; \
-  cmake ../.. -DCMAKE_TOOLCHAIN_FILE="../../$TOOLCHAIN" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"; \
+  (mkdir -p "build/$MCU/$BUILD_TYPE"; cd "build/$MCU/$BUILD_TYPE"; \
+  cmake ../../.. -DCMAKE_TOOLCHAIN_FILE="../../../$TOOLCHAIN" -DMCU="$MCU" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"; \
   make)
 done
