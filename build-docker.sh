43a642eaee6d1350f734f8a379279f0d6fb46269
Falko Zurell <falko.zurell@ubirch.com>
2016-05-21 00:00:17 +0200

    fixing Docker build

--- build-docker.sh
+++ build-docker.sh
@@ -19,7 +19,7 @@ function init() {
 }
 
 function build_software() {
-  docker run --user `id -u`:`id -g` -e "HOME=/build" --workdir=/build/ubirch-wolfssl-package --volume=${PWD}/..:/build --env=${TOOLCHAIN} --env=${WOLFSSL_ROOT} --entrypoint=/bin/bash ubirch/arm-build:${ARM_CONTAINER_VERSION} ./build.sh
+  docker run --user `id -u`:`id -g` -e "HOME=/build" --workdir=/build/ubirch-wolfssl-package --volume=${PWD}/..:/build -e "TOOLCHAIN=${TOOLCHAIN}" -e "WOLFSSL_ROOT=${WOLFSSL_ROOT}" --entrypoint=/bin/bash ubirch/arm-build:${ARM_CONTAINER_VERSION} ./build.sh
   if [ $? -ne 0 ]; then
       echo "Docker build failed"
       exit 1
