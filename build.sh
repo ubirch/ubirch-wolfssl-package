1d64b0323123f87968809ea70260722a3fecc0b1
Falko Zurell <falko.zurell@ubirch.com>
2016-05-20 23:18:19 +0200

    added Docker build files

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-#! /bin/sh
-TOOLCHAIN=../ubirch-arm-toolchain/cmake/ubirch-arm-gcc-toolchain.cmake
+#! /bin/bash
+TOOLCHAIN=${TOOLCHAIN:-"../ubirch-arm-toolchain/cmake/ubirch-arm-gcc-toolchain.cmake"}
+WOLFSSL_ROOT=${WOLFSSL_ROOT:-"../ubirch-wolfssl"}
 MCU=MK82F25615
 # do an out-of-source build for all configurations
 if [ "$1" == "-a" ]
@@ -10,6 +11,6 @@ fi
 for BUILD_TYPE in $BUILDS
 do
   (mkdir -p "build/$MCU/$BUILD_TYPE"; cd "build/$MCU/$BUILD_TYPE"; \
-  cmake ../../.. -DCMAKE_TOOLCHAIN_FILE="../../../$TOOLCHAIN" -DMCU="$MCU" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"; \
+  cmake ../../.. -DCMAKE_TOOLCHAIN_FILE="../../../$TOOLCHAIN" -DMCU="$MCU" -DWOLFSSL_ROOT="$WOLFSSL_ROOT" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"; \
   make)
 done
